4df7cacfa9b4020a70953a58b571809b29440f95
HAO <xhspiderman@gmail.com>
2021-10-23 15:15:43 -1000

    first commit

---
+++ src/main/java/xuhaoace/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,11 @@
+package xuhaoace.ppmtool.repositories;
+
+import xuhaoace.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+}
