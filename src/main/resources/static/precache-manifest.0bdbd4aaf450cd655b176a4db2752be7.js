0466039cdc54621a80aceaca5e7741b206fc8920
HAO <xhspiderman@gmail.com>
2021-10-23 17:43:58 -1000

    added react frontend client

---
+++ src/main/resources/static/precache-manifest.0bdbd4aaf450cd655b176a4db2752be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a399f802bd6994aca6baa3f3896b08cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33131edf6695a5caef6f",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5c0b5a5d5efb9fe669a5",
+    "url": "/static/css/main.b96a4214.chunk.css"
+  },
+  {
+    "revision": "33131edf6695a5caef6f",
+    "url": "/static/js/2.9f99fef1.chunk.js"
+  },
+  {
+    "revision": "5c0b5a5d5efb9fe669a5",
+    "url": "/static/js/main.b75d5a9b.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
